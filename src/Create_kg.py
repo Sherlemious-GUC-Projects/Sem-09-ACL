7f83fcb904062fadef60364bed08111d917c9ccd
Omar-Goba <omaryasserassem1@gmail.com>
2025-11-26 17:09:57 +0200

    Feat: do the feature extraction

--- src/Create_kg.py
+++ src/Create_kg.py
@@ -1,6 +1,7 @@
 ### ~~~ GLOBAL IMPORTS ~~~ ###
 from dataclasses import dataclass
 import pandas as pd
+import uuid
 import os
 
 
@@ -77,15 +78,142 @@ def load_data(path: str = "./dbs/Airline_surveys_sample.csv") -> pd.DataFrame:
     return df
 
 
+def construct_passanger_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Construct passenger data from the given DataFrame.
+    1. for each passenger, create a unique identifier
+    2. extracts as well their loyalty_program_level
+    3. and finally adds the generation
+    Args:
+        df (pd.DataFrame): DataFrame containing the original data.
+    Returns:
+        pd.DataFrame: DataFrame containing passenger_id,
+                      loyalty_program_level, and generation.
+    Throws:
+        None
+    Note:
+        An important assumption here is that each row in the dataframe
+        corresponds to a unique passenger.
+    """
+    ### get all unique passengers ###
+    unique_passengers: pd.DataFrame = df[["loyalty_program_level", "generation"]].copy()
+
+    ### add unique identifier ###
+    unique_passengers["passenger_id"] = [
+        str(uuid.uuid4()) for _ in range(unique_passengers.shape[0])
+    ]
+
+    return unique_passengers
+
+
+def construct_journey_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Construct journey data from the given DataFrame.
+    1. for each journey, create a unique identifier
+    2. extracts relevant journey attributes
+    Args:
+        df (pd.DataFrame): DataFrame containing the original data.
+    Returns:
+        pd.DataFrame: DataFrame containing feedback_ID,
+                      food_satisfaction_score, arrival_delay_minutes,
+                      actual_flown_miles, number_of_legs, passenger_class.
+    Throws:
+        None
+    """
+    ### get all unique journeys ###
+    unique_journeys: pd.DataFrame = df[
+        [
+            "feedback_ID",
+            "food_satisfaction_score",
+            "arrival_delay_minutes",
+            "actual_flown_miles",
+            "number_of_legs",
+            "passenger_class",
+        ]
+    ].copy()
+
+    ### add unique identifier ###
+
+    return unique_journeys
+
+
+def construct_flight_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Construct flight data from the given DataFrame.
+    1. for each flight, create a unique identifier
+    2. extracts relevant flight attributes
+    Args:
+        df (pd.DataFrame): DataFrame containing the original data.
+    Returns:
+        pd.DataFrame: DataFrame containing flight_number,
+                      fleet_type_description.
+    Throws:
+        None
+    """
+    ### get all unique flights ###
+    unique_flights: pd.DataFrame = df[
+        ["flight_number", "fleet_type_description"]
+    ].copy()
+
+    ### add unique identifier ###
+
+    return unique_flights
+
+
+def construct_airport_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Construct airport data from the given DataFrame.
+    1. for each airport, create a unique identifier
+    2. extracts relevant airport attributes
+    Args:
+        df (pd.DataFrame): DataFrame containing the original data.
+    Returns:
+        pd.DataFrame: DataFrame containing station_code,
+                      airport_name, city, state, country.
+    Throws:
+        None
+    """
+    ### get all unique airports ###
+    origin_airports: pd.DataFrame = df[
+        [
+            "origin_station_code",
+        ]
+    ].copy()
+    destination_airports: pd.DataFrame = df[
+        [
+            "destination_station_code",
+        ]
+    ].copy()
+
+    unique_airports: pd.DataFrame = (
+        pd.concat([origin_airports, destination_airports])
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+
+    return unique_airports
+
+
 def main() -> int:
     """ """
     ### load config ###
-    config = load_config()
+    # config = load_config()
 
     ### load data ###
     df = load_data()
 
-    print(df.head())
+    ### construct passenger data ###
+    passenger_data = construct_passanger_data(df)
+
+    ### construct journey data ###
+    journey_data = construct_journey_data(df)
+
+    ### construct flight data ###
+    flight_data = construct_flight_data(df)
+
+    ### construct airport data ###
+    airport_data = construct_airport_data(df)
+
     return 0
 
 
